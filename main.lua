098e35a0e8762fea6181d0d145a07ed4a804ac49
Letr0b1t <letr0b1t@protonmail.com>
2023-06-16 18:01:18 +0200

    Update main.lua
    Added a new response button.

--- main.lua
+++ main.lua
@@ -1,21 +1,37 @@
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_LOGIN")
 
+local MoanSound = "Interface\\Addons\\MyFugginAddon\\sounds\\moan.mp3"
+
 local function OnEvent(self, event, ...)
     if event == "PLAYER_LOGIN" then
         StaticPopup_Show("WELCOME_MESSAGE")
     end
 end
 
-local MoanSound = "Interface\\Addons\\MyFugginAddon\\sounds\\moan.mp3"
-
 frame:SetScript("OnEvent", OnEvent)
 
 StaticPopupDialogs["WELCOME_MESSAGE"] = {
     text = "Mirko stinkt!",
-    button1 = "OK",
+    button1 = "Stimmt",
+    button2 = "Nein...",
     OnAccept = function()
         PlaySoundFile(MoanSound, "MASTER")
+    end,
+    OnCancel = function()
+        StaticPopup_Show("SMELLS_OF_CURSE")
+    end,
+    timeout = 0,
+    whileDead = true,
+    hideOnEscape = true,
+    preferredIndex = 3 
+}
+
+StaticPopupDialogs["SMELLS_OF_CURSE"] = {
+    text = "Doch!",
+    button1 = "OK",
+    OnAccept = function()
+        
     end,
     timeout = 0,
     whileDead = true,
