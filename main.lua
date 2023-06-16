0b011a9650e53b827be325a3dee50e7b60003a8f
Letr0b1t <letr0b1t@protonmail.com>
2023-06-16 17:15:37 +0200

    Updated Sourcecode
    Updated the functionality of the main function.

--- main.lua
+++ main.lua
@@ -3,15 +3,15 @@ frame:RegisterEvent("PLAYER_LOGIN")
 
 local function OnEvent(self, event, ...)
     if event == "PLAYER_LOGIN" then
-        StaticPopup_Show("MEIN_ADDON_MESSAGE")
+        StaticPopup_Show("WELCOME_MESSAGE")
     end
 end
 
-local MoanSound = "Interface\\Addons\\MyFugginAddon\\moan.mp3"
+local MoanSound = "Interface\\Addons\\MyFugginAddon\\sounds\\moan.mp3"
 
 frame:SetScript("OnEvent", OnEvent)
 
-StaticPopupDialogs["MEIN_ADDON_MESSAGE"] = {
+StaticPopupDialogs["WELCOME_MESSAGE"] = {
     text = "Mirko stinkt!",
     button1 = "OK",
     OnAccept = function()
@@ -20,5 +20,5 @@ StaticPopupDialogs["MEIN_ADDON_MESSAGE"] = {
     timeout = 0,
     whileDead = true,
     hideOnEscape = true,
-    preferredIndex = 3 -- Setze die Reihenfolge, falls weitere Dialoge geöffnet sind
+    preferredIndex = 3 
 }
