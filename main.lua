677a1dbade9e7601b4763e9f584e9aef1d0e498f
Letr0b1t <letr0b1t@protonmail.com>
2023-06-16 17:12:08 +0200

    Add files via upload
    Updated the main function for some extra features!

--- main.lua
+++ main.lua
@@ -1,1 +1,24 @@
-message('Mirko Stink!')
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_LOGIN")
+
+local function OnEvent(self, event, ...)
+    if event == "PLAYER_LOGIN" then
+        StaticPopup_Show("MEIN_ADDON_MESSAGE")
+    end
+end
+
+local MoanSound = "Interface\\Addons\\MyFugginAddon\\moan.mp3"
+
+frame:SetScript("OnEvent", OnEvent)
+
+StaticPopupDialogs["MEIN_ADDON_MESSAGE"] = {
+    text = "Mirko stinkt!",
+    button1 = "OK",
+    OnAccept = function()
+        PlaySoundFile(MoanSound, "MASTER")
+    end,
+    timeout = 0,
+    whileDead = true,
+    hideOnEscape = true,
+    preferredIndex = 3 -- Setze die Reihenfolge, falls weitere Dialoge geöffnet sind
+}
